5fffabec6a0c7aeb962d0d582a5b21485cb165c1
mpascualach <miguelpacheson@hotmail.es>
2023-09-04 16:01:32 +0200

    CharacterDetails.vue establecido

--- src/components/CharacterDetails.vue
+++ src/components/CharacterDetails.vue
@@ -1,1 +1,20 @@
-<template></template>
+<template>
+  <div class="character-viewer"></div>
+</template>
+
+<script setup>
+import { useCharacterStore } from "../stores/characterStore";
+import { watch } from "vue";
+
+const characterStore = useCharacterStore();
+
+watch(
+  () => characterStore.selectedCharacter,
+  (newSelectedCharacter, oldSelectedCharacter) => {
+    console.log("Showing deets for: ", newSelectedCharacter);
+    console.log("old character here: ", oldSelectedCharacter);
+  }
+);
+</script>
+
+<style scoped></style>
