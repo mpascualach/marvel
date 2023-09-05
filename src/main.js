d7f360e094a1ccdd23b445fdbe68c42eb3352a94
mpascualach <miguelpacheson@hotmail.es>
2023-09-05 15:10:24 +0200

    me di cuenta que lo anterior ya lo hacia por defecto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import App from "./App.vue";
 
 import { createApp } from "vue";
-import vuetify from "./vuetify.theme.js";
 
 import { registerPlugins } from "@/plugins";
 import { createPinia } from "pinia";
@@ -9,7 +8,7 @@ import { createPinia } from "pinia";
 const pinia = createPinia();
 
 const app = createApp(App).use(pinia);
-app.use(vuetify);
+
 registerPlugins(app);
 
 app.mount("#app");
