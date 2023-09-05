58d3eba7c3c9e3154be2b684d1a5b23c31f1d536
mpascualach <miguelpacheson@hotmail.es>
2023-09-05 18:34:10 +0200

    algunas cosas vuetify mas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app>
+    <v-btn icon @click="toggleTheme" class="theme-toggle-button">
+      <v-icon>{{
+        darkMode ? "mdi-white-balance-sunny" : "mdi-moon-waxing-crescent"
+      }}</v-icon>
+    </v-btn>
     <v-main>
       <Select />
       <!-- <p>Selected character: {{ selectedCharacter.name }}</p> -->
@@ -12,11 +17,29 @@
 import Select from "./components/character/Select.vue";
 import CharacterViewer from "./CharacterViewer.vue";
 
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import vuetify from "./plugins/vuetify";
+
+const darkMode = ref(false);
 
 onMounted(() => {
   document.title = "Marvel Character Viewer";
 });
+
+const toggleTheme = () => {
+  console.log("toggleTheme");
+  darkMode.value = !darkMode.value;
+  console.log(vuetify);
+  vuetify.framework.theme.dark = darkMode.value;
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.theme-toggle-button {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+  z-index: 1;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,8 +8,28 @@ export default createVuetify({
     themes: {
       light: {
         colors: {
-          primary: "#1867C0",
-          secondary: "#5CBBF6",
+          primary: "#1976D2", // Deep blue shade
+          secondary: "#9C27B0", // Purple shade
+          accent: "#FF5722", // Deep orange shade for accents
+          background: "#F5F5F5", // Light gray background color
+          surface: "#FFFFFF", // White surface color
+          error: "#FF5252", // Red shade for errors
+          info: "#2196F3", // Blue shade for info
+          success: "#4CAF50", // Green shade for success
+          warning: "#FFC107", // Yellow shade for warnings
+        },
+      },
+      dark: {
+        colors: {
+          primary: "#2196F3", // A deep blue shade
+          secondary: "#03DAC6", // A teal shade
+          accent: "#FF4081", // A pink shade for accents
+          background: "#121212", // Dark background color
+          surface: "#1E1E1E", // Slightly lighter surface color
+          error: "#FF5252", // Red shade for errors
+          info: "#2196F3", // Reusing primary color for info
+          success: "#4CAF50", // Green shade for success
+          warning: "#FFC107", // Yellow shade for warnings
         },
       },
     },
