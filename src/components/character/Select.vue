0d27829dbad0f56ab4c72d67ee948374787aa851
mpascualach <miguelpacheson@hotmail.es>
2023-09-05 15:47:26 +0200

    maquetacion del input

--- src/components/character/Select.vue
+++ src/components/character/Select.vue
@@ -2,15 +2,19 @@
   <div class="select-holder">
     <v-select
       v-model="characterStore.selectedCharacter"
-      label="Elige un personaje"
       :items="characterStore.characters"
       item-title="name"
       item-value="id"
       variant="outlined"
       @update:modelValue="handleCharacterSelection"
     >
-      <input />
     </v-select>
+    <v-text-field
+      class="character-searcher"
+      v-model="searchKeyword"
+      placeholder="Busca o selecciona tu personaje"
+      variant="outlined"
+    ></v-text-field>
   </div>
   <p>{{ err }}</p>
 </template>
@@ -19,22 +23,27 @@
 import { useCharacterStore } from "@/stores/CharacterStore";
 import { onMounted } from "vue";
 import { mande } from "mande";
+import { debounce } from "lodash";
 
 const characterStore = useCharacterStore();
 
 const marvel = mande("https://gateway.marvel.com/v1/public", {
   apikey: import.meta.env.VITE_PUBLIC_KEY,
 });
-let err;
+let err, searchKeyword;
+
+const searchCharactersDebounced = debounce(searchCharacters, 300);
 
 onMounted(async () => {
   // iniciar listado de personajes - se limita a 20 por defecto en el API
   try {
-    const { data } = await marvel.get(
-      `/characters?apikey=${import.meta.env.VITE_PUBLIC_KEY}`
-    );
-    characterStore.characters = data.results;
-    console.log("Characters: ", data.results);
+    if (!searchKeyword) {
+      const { data } = await marvel.get(
+        `/characters?apikey=${import.meta.env.VITE_PUBLIC_KEY}`
+      );
+      characterStore.characters = data.results;
+      console.log("Characters: ", data.results);
+    }
   } catch (error) {
     err = error;
     console.error(error);
@@ -49,11 +58,32 @@ const handleCharacterSelection = (selectedValue) => {
   // reseteando el entry - por si se había establecido antes
   characterStore.entrySelected = false;
 };
+
+const searchCharacters = async () => {
+  try {
+    const { data } = await marvel.get(
+      `/characters?nameStartsWith=${searchKeyword}&apikey=${
+        import.meta.env.VITE_PUBLIC_KEY
+      }`
+    );
+  } catch (error) {
+    err = error;
+    console.log(error);
+  }
+};
 </script>
 
 <style scoped>
 .select-holder {
   padding: 40px;
   padding-bottom: 10px;
+  position: relative;
+}
+
+.character-searcher {
+  position: absolute;
+  width: 80%;
+  top: 30%;
+  left: 5%;
 }
 </style>
